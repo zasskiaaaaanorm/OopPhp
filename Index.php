751d6968dd79144a5b63d83be4c834f24836a458
saskia <saskia.norma@koding.sch.id>
2019-09-12 14:44:19 +0700

    first php

---
+++ Index.php
@@ -0,0 +1,17 @@
+<?php
+//class
+class Sekolah
+{
+    //property
+    public $sekolahan;
+
+    //method
+    public function getSekolahan($sekolahan)
+    {
+        return $sekolahan;
+    }
+}
+
+//object
+$sekolahan = new Sekolah;
+echo $sekolahan->getSekolahan('Skamuhi Pink');# OopPhp
